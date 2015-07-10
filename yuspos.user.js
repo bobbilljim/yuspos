dae4182545fee335ce104b98bee07d06a1d14571
bobbilljim <bob.bill.jim@gmail.com>
2015-07-10 16:25:51 +1200

    bump version

--- yuspos.user.js
+++ yuspos.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SA forums shit
 // @namespace    bobbilljim.com
-// @version      1.05
+// @version      1.06
 // @description  sa forums shit
 // @author       You
 // @match        http://forums.somethingawful.com/*
